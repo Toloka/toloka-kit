3aa2e6189893d6053eac2844b31e3ebc832536d9
lehasm <alexey-sm@yandex-team.ru>
2022-04-18 22:47:59 +0300

    draft

--- src/client/actions.py
+++ src/client/actions.py
@@ -37,17 +37,17 @@ class RuleAction(BaseParameters, spec_enum=RuleType, spec_field='type'):
 
 
 class Restriction(RuleAction, spec_value=RuleType.RESTRICTION):
-    """Block access to projects or pools
+    """Restricts performer's access to projects or pools.
 
-    It's better to use new version: RestrictionV2.
+    To have better control over restriction period use `RestrictionV2`.
 
     Attributes:
         parameters.scope:
-            * POOL - Current pool where this rule was triggered. Does not affect the user's rating.
-            * PROJECT - Current project where this rule was triggered. Affects the user's rating.
-            * ALL_PROJECTS - All customer's projects.
-        parameters.duration_days: Blocking period in days. By default, the lock is indefinite.
-        parameters.private_comment: Comment (reason for blocking). Available only to the customer.
+            * `POOL` - A performer can not access the pool if the action is applied. In this case the performer's rating is not affected.
+            * `PROJECT` - A performer can not access the entire project containing the pool. The performer's rating is affected.
+            * `ALL_PROJECTS` - A performer can not access any customer's project.
+        parameters.duration_days: Blocking period in days. By default, the block is permanent.
+        parameters.private_comment: A private comment. It is visible only for the customer.
     """
 
     class Parameters(RuleAction.Parameters):
@@ -57,30 +57,31 @@ class Restriction(RuleAction, spec_value=RuleType.RESTRICTION):
 
 
 class RestrictionV2(RuleAction, spec_value=RuleType.RESTRICTION_V2):
-    """Block access to projects or pools
+    """Restricts performer's access to projects or pools.
 
     Attributes:
         parameters.scope:
-            * POOL - Current pool where this rule was triggered. Does not affect the user's rating.
-            * PROJECT - Current project where this rule was triggered. Affects the user's rating.
-            * ALL_PROJECTS - All customer's projects.
-        parameters.duration: Blocking period in duration_unit.
-        parameters.duration_unit: In what units the restriction duration is measured:
-            * MINUTES
-            * HOURS
-            * DAYS
-            * PERMANENT
-        parameters.private_comment: Comment (reason for blocking). Available only to the customer.
+            * `POOL` - A performer can not access the pool if the action is applied. In this case the performer's rating is not affected.
+            * `PROJECT` - A performer can not access the entire project containing the pool. The performer's rating is affected.
+            * `ALL_PROJECTS` - A performer can not access any customer's project.
+        parameters.duration: The duration of the block period measured in `duration_unit`.
+        parameters.duration_unit: 
+            * `MINUTES`;
+            * `HOURS`;
+            * `DAYS`;
+            * `PERMANENT`.
+            `PERMANENT` means that block is is permanent.
+        parameters.private_comment: A private comment. It is visible only for the customer.
 
     Example:
-        How to ban performers who answers too fast.
+        The following quality control rule disallows access to the project for 10 days, if a performer answers too fast.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
         >>>     collector=toloka.collectors.AssignmentSubmitTime(history_size=5, fast_submit_threshold_seconds=20),
         >>>     conditions=[toloka.conditions.FastSubmittedCount > 1],
         >>>     action=toloka.actions.RestrictionV2(
-        >>>         scope=toloka.user_restriction.UserRestriction.PROJECT,
+        >>>         scope='PROJECT',
         >>>         duration=10,
         >>>         duration_unit='DAYS',
         >>>         private_comment='Fast responses',
@@ -97,18 +98,18 @@ class RestrictionV2(RuleAction, spec_value=RuleType.RESTRICTION_V2):
 
 
 class SetSkillFromOutputField(RuleAction, spec_value=RuleType.SET_SKILL_FROM_OUTPUT_FIELD):
-    """Set performer skill value from source
+    """Sets performer's skill value to the percentage of correct or incorrect answers.
 
-    You can use this rule only with collectors.MajorityVote and collectors.GoldenSet.
+    This action can be used with [collectors.MajorityVote](https://toloka.ai/en/docs/toloka-kit/reference/toloka.client.collectors.MajorityVote) and [collectors.GoldenSet](https://toloka.ai/en/docs/toloka-kit/reference/toloka.client.collectors.GoldenSet).
 
     Attributes:
         parameters.skill_id: ID of the skill to update.
         parameters.from_field: The value to assign to the skill:
-            * correct_answers_rate - Percentage of correct answers.
-            * incorrect_answer_rate - Percentage of incorrect answers.
+            * `correct_answers_rate` - Percentage of correct answers.
+            * `incorrect_answers_rate` - Percentage of incorrect answers.
 
     Example:
-        How to set the skill value to mean consistency with the majority.
+        In the following example `MajorityVote` is used to update a skill value.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
@@ -130,19 +131,18 @@ class SetSkillFromOutputField(RuleAction, spec_value=RuleType.SET_SKILL_FROM_OUT
 
 
 class ChangeOverlap(RuleAction, spec_value=RuleType.CHANGE_OVERLAP):
-    """Increase the overlap of the set of tasks (or tasks, if the option is used "smart mixing")
+    """Increases the overlap of a task.
 
-    You can use this rule only with collectors.UsersAssessment and collectors.AssignmentsAssessment.
+    You can use this rule only with [collectors.UsersAssessment](https://toloka.ai/en/docs/toloka-kit/reference/toloka.client.collectors.UsersAssessment) and [collectors.AssignmentsAssessment](https://toloka.ai/en/docs/toloka-kit/reference/toloka.client.collectors.AssignmentsAssessment).
 
     Attributes:
-        parameters.delta: The number by which you want to increase the overlap of the task set
-            (or the task if the option is used "smart mixing").
-        parameters.open_pool: Changing the pool status:
-            * True - Open the pool after changing if it is closed.
-            * False - Do not open the pool after the change if it is closed.
+        parameters.delta: An overlap increment.
+        parameters.open_pool:
+            * True — Open the pool after changing the overlap value.
+            * False — Do not open the pool if it is already closed.
 
     Example:
-        How to increase task overlap when you reject assignment in delayed mode.
+        The example shows, how to increase task overlap when you reject assignments manually.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
@@ -159,14 +159,14 @@ class ChangeOverlap(RuleAction, spec_value=RuleType.CHANGE_OVERLAP):
 
 
 class SetSkill(RuleAction, spec_value=RuleType.SET_SKILL):
-    """Set performer skill value
+    """Sets performer's skill value.
 
     Attributes:
-        parameters.skill_id: ID of the skill to update.
-        parameters.skill_value: The value to be assigned to the skill.
+        parameters.skill_id: The ID of the skill.
+        parameters.skill_value: The new value of the skill.
 
     Example:
-        How to mark performers completing a task so that you can filter them later in the checking project.
+        When an answer is accepted, assign a skill to the performer. Later you can filter performers by that skill.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
@@ -183,21 +183,21 @@ class SetSkill(RuleAction, spec_value=RuleType.SET_SKILL):
 
 
 class RejectAllAssignments(RuleAction, spec_value=RuleType.REJECT_ALL_ASSIGNMENTS):
-    """Reject all replies from the performer. Only for pools with post acceptance.
+    """Rejects all performer's assignments in the pool. This action is available for pools with non-automatic acceptance.
 
     The performer is not explicitly installed, the rejection occurs on the performer on which the rule will be triggered.
 
     Attributes:
-        parameters.public_comment: Describes why you reject all assignments from this performer.
+        parameters.public_comment: The reason of the rejection. It is visible to the customer and to the performer.
 
     Example:
-        How to reject all assignments if performer sends answers too fast (only for pools with post acceptance).
+        Reject all assignments if a performer sends responses too fast. Note, that the pool must be configured with non-automatic response acceptance.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
         >>>     collector=toloka.collectors.AssignmentSubmitTime(history_size=5, fast_submit_threshold_seconds=20),
         >>>     conditions=[toloka.conditions.FastSubmittedCount > 3],
-        >>>     action=toloka.actions.RejectAllAssignments(public_comment='Too fast answering. You are cheater!')
+        >>>     action=toloka.actions.RejectAllAssignments(public_comment='Too fast responses.')
         >>> )
         ...
     """
@@ -207,12 +207,12 @@ class RejectAllAssignments(RuleAction, spec_value=RuleType.REJECT_ALL_ASSIGNMENT
 
 
 class ApproveAllAssignments(RuleAction, spec_value=RuleType.APPROVE_ALL_ASSIGNMENTS):
-    """Approve all replies from the performer
+    """Accepts all performer's assignments in the pool.
 
     The performer is not explicitly installed, the approval occurs on the performer on which the rule will be triggered.
 
     Example:
-        How to approve all assignments if performer doing well with golden tasks.
+        Accept all assignments if a performer gives correct responses for golden tasks. Note, that the pool must be configured with non-automatic response acceptance.
 
         >>> new_pool = toloka.pool.Pool(....)
         >>> new_pool.quality_control.add_action(
