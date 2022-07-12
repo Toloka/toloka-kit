76175e58851e6488ae2b1fa7b88d58c4ae8d985c
alexdrydew <aleksey.suhorosov@gmail.com>
2022-07-12 16:13:09 +0300

    update tests

--- tests/template_builder/test_examples.py
+++ tests/template_builder/test_examples.py
@@ -10,4 +10,5 @@ from toloka.client.project.view_spec import ViewSpec
 def test_examples(example_view_spec):
     result = ViewSpec.structure(example_view_spec).unstructure()
     assert json.loads(result.pop('config'))['plugins'] == json.loads(example_view_spec.pop('config'))['plugins']
+    example_view_spec.setdefault('inferDataSpec', False)
     assert result == example_view_spec
